84918c3c0165847ce6e67e5a7eb374f813a0d923
mykola <mbilokonsky@gmail.com>
2020-05-12 16:04:37 -0400

    no need to expose angular or flamingo through nginx right I think

--- mock.js
+++ mock.js
@@ -1,8 +1,6 @@
 // content of index.js
 const http = require("http");
 const WEBAPP_PORT = 3000;
-const FLAMINGO_PORT = 3001;
-const ANGULAR_PORT = 3002;
 
 const handlerBuilder = (name) => {
   return (request, response) => {
@@ -11,26 +9,10 @@ const handlerBuilder = (name) => {
 };
 
 const webappServer = http.createServer(handlerBuilder("WEBAPP"));
-const flamingoServer = http.createServer(handlerBuilder("FLAMINGO"));
-const angularServer = http.createServer(handlerBuilder("ANGULAR"));
 
 webappServer.listen(WEBAPP_PORT, (err) => {
   if (err) {
     console.err("Error running WEBAPP", err);
   }
   console.log(`WEBAPP server is listening on ${WEBAPP_PORT}`);
-});
-
-flamingoServer.listen(FLAMINGO_PORT, (err) => {
-  if (err) {
-    console.err("Error running FLAMINGO", err);
-  }
-  console.log(`FLAMINGO server is listening on ${FLAMINGO_PORT}`);
-});
-
-angularServer.listen(ANGULAR_PORT, (err) => {
-  if (err) {
-    console.err("Error running ANGULAR", err);
-  }
-  console.log(`ANGULAR server is listening on ${ANGULAR_PORT}`);
 });
